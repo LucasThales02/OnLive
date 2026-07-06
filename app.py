e8bab313ae7e939d0a59a78e5609da9fb42c2b48
Lucas Thales <thtitecno@gmail.com>
2026-07-06 11:18:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from supabase import create_client
+from flask import Flask, request, jsonify
+import os
 
+app = Flask(__name__)
+
+supabase = create_client(
+    os.environ["SUPABASE_URL"],
+    os.environ["SUPABASE_KEY"]
+)
