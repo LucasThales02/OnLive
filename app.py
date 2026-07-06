fede093c4cdd58d1cf1f48af4ee3a63d9d65e5ba
Lucas Thales <thtitecno@gmail.com>
2026-07-06 12:23:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,47 @@ def player_api():
 
     username = request.args.get("username")
     password = request.args.get("password")
+    action = request.args.get("action")
 
-    return jsonify({
-        "user_info": {
-            "auth": 1,
-            "username": username
-        }
-    })
+    # Login sem action
+    if not action:
 
-if __name__ == "__main__":
-    app.run()
+        return jsonify({
+            "user_info": {
+                "auth": 1,
+                "status": "Active",
+                "username": username,
+                "password": password,
+                "active_cons": "1",
+                "max_connections": "1"
+            }
+        })
+
+    # Categorias Live TV
+    if action == "get_live_categories":
+
+        return jsonify([
+            {
+                "category_id": "1",
+                "category_name": "ABERTA"
+            },
+            {
+                "category_id": "2",
+                "category_name": "ESPORTES"
+            }
+        ])
+
+    # Canais Live TV
+    if action == "get_live_streams":
+
+        return jsonify([
+            {
+                "stream_id": 1,
+                "name": "Globo SP",
+                "stream_icon": "",
+                "category_id": "1",
+                "stream_type": "live'
+            }
+        ])
+
+    return jsonify([])
